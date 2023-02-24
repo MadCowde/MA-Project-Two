2768553ba1ff575cc5607406012ee510466f0683
Brendan <mirageofthepast@hotmail.com>
2023-02-23 19:45:19 -0600

    Update to depency to create tables if necessary

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/entities/AccountTable.java
@@ -0,0 +1,11 @@
+package com.techelevator.tenmo.entities;
+
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.Entity;
+
+@Entity
+@Table
+public class AccountTable {
+}
